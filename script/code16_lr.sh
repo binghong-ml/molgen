ca67f16b91eae4538e80dfb3b993eebbf1bbd254
Sungsoo <sungsahn0215@gmail.com>
2021-09-11 00:14:18 +0400

    tmp

---
+++ script/code16_lr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python train_generator.py --vq_vocab_size 16 --lr 1e-5 --tag code16_lr
---
+++ script/code32_lr.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python train_generator.py --vq_vocab_size 32 --lr 1e-5 --tag code32_lr
