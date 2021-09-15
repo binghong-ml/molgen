939f7a23fbf00b6c76919046e64d0f4f74dc187c
Sungsoo <sungsahn0215@gmail.com>
2021-09-15 14:05:51 +0400

    asd

--- src/data/smiles.py
+++ src/data/smiles.py
@@ -3,93 +3,74 @@
 
 import networkx as nx
 from rdkit import Chem
+from rdkit.Chem.rdchem import ChiralType, BondType, BondDir
 
-ATOM_TOKENS = {
-    "<C0>": (6, "CHI_TETRAHEDRAL_CCW", 0, 0),
-    "<C1>": (6, "CHI_TETRAHEDRAL_CCW", 0, 1),
-    "<C2>": (6, "CHI_TETRAHEDRAL_CW", 0, 0),
-    "<C3>": (6, "CHI_TETRAHEDRAL_CW", 0, 1),
-    "<C4>": (6, "CHI_UNSPECIFIED", -1, 1),
-    "<C5>": (6, "CHI_UNSPECIFIED", -1, 2),
-    "<C6>": (6, "CHI_UNSPECIFIED", 0, 0),
-    "<N0>": (7, "CHI_UNSPECIFIED", -1, 0),
-    "<N1>": (7, "CHI_UNSPECIFIED", -1, 1),
-    "<N2>": (7, "CHI_UNSPECIFIED", 0, 0),
-    "<N3>": (7, "CHI_UNSPECIFIED", 0, 1),
-    "<N4>": (7, "CHI_UNSPECIFIED", 1, 0),
-    "<N5>": (7, "CHI_UNSPECIFIED", 1, 1),
-    "<N6>": (7, "CHI_UNSPECIFIED", 1, 2),
-    "<N7>": (7, "CHI_UNSPECIFIED", 1, 3),
-    "<O0>": (8, "CHI_UNSPECIFIED", -1, 0),
-    "<O1>": (8, "CHI_UNSPECIFIED", 0, 0),
-    "<O2>": (8, "CHI_UNSPECIFIED", 1, 0),
-    "<O3>": (8, "CHI_UNSPECIFIED", 1, 1),
-    "<F0>": (9, "CHI_UNSPECIFIED", 0, 0),
-    "<P0>": (15, "CHI_TETRAHEDRAL_CCW", 0, 0),
-    "<P1>": (15, "CHI_TETRAHEDRAL_CW", 0, 0),
-    "<P2>": (15, "CHI_TETRAHEDRAL_CW", 0, 1),
-    "<P3>": (15, "CHI_UNSPECIFIED", 0, 0),
-    "<P4>": (15, "CHI_UNSPECIFIED", 0, 1),
-    "<P5>": (15, "CHI_UNSPECIFIED", 0, 2),
-    "<P6>": (15, "CHI_UNSPECIFIED", 1, 0),
-    "<P7>": (15, "CHI_UNSPECIFIED", 1, 1),
-    "<>": (16, "CHI_TETRAHEDRAL_CCW", 0, 0),
-    "<>": (16, "CHI_TETRAHEDRAL_CW", 0, 0),
-    "<>": (16, "CHI_TETRAHEDRAL_CW", 1, 0),
-    "<>": (16, "CHI_UNSPECIFIED", -1, 0),
-    "<>": (16, "CHI_UNSPECIFIED", 0, 0),
-    "<>": (16, "CHI_UNSPECIFIED", 1, 0),
-    "<>": (16, "CHI_UNSPECIFIED", 1, 1),
-    "<>": (17, "CHI_UNSPECIFIED", 0, 0),
-    "<>": (35, "CHI_UNSPECIFIED", 0, 0),
-    "<>": (53, "CHI_UNSPECIFIED", 0, 0),
+TOKEN2ATOMFEAT = {
+    "<C0>": (6, ChiralType.CHI_TETRAHEDRAL_CCW, 0, 0),
+    "<C1>": (6, ChiralType.CHI_TETRAHEDRAL_CCW, 0, 1),
+    "<C2>": (6, ChiralType.CHI_TETRAHEDRAL_CW, 0, 0),
+    "<C3>": (6, ChiralType.CHI_TETRAHEDRAL_CW, 0, 1),
+    "<C4>": (6, ChiralType.CHI_UNSPECIFIED, -1, 1),
+    "<C5>": (6, ChiralType.CHI_UNSPECIFIED, -1, 2),
+    "<C6>": (6, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<N0>": (7, ChiralType.CHI_UNSPECIFIED, -1, 0),
+    "<N1>": (7, ChiralType.CHI_UNSPECIFIED, -1, 1),
+    "<N2>": (7, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<N3>": (7, ChiralType.CHI_UNSPECIFIED, 0, 1),
+    "<N4>": (7, ChiralType.CHI_UNSPECIFIED, 1, 0),
+    "<N5>": (7, ChiralType.CHI_UNSPECIFIED, 1, 1),
+    "<N6>": (7, ChiralType.CHI_UNSPECIFIED, 1, 2),
+    "<N7>": (7, ChiralType.CHI_UNSPECIFIED, 1, 3),
+    "<O0>": (8, ChiralType.CHI_UNSPECIFIED, -1, 0),
+    "<O1>": (8, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<O2>": (8, ChiralType.CHI_UNSPECIFIED, 1, 0),
+    "<O3>": (8, ChiralType.CHI_UNSPECIFIED, 1, 1),
+    "<F0>": (9, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<P0>": (15, ChiralType.CHI_TETRAHEDRAL_CCW, 0, 0),
+    "<P1>": (15, ChiralType.CHI_TETRAHEDRAL_CW, 0, 0),
+    "<P2>": (15, ChiralType.CHI_TETRAHEDRAL_CW, 0, 1),
+    "<P3>": (15, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<P4>": (15, ChiralType.CHI_UNSPECIFIED, 0, 1),
+    "<P5>": (15, ChiralType.CHI_UNSPECIFIED, 0, 2),
+    "<P6>": (15, ChiralType.CHI_UNSPECIFIED, 1, 0),
+    "<P7>": (15, ChiralType.CHI_UNSPECIFIED, 1, 1),
+    "<S0>": (16, ChiralType.CHI_TETRAHEDRAL_CCW, 0, 0),
+    "<S1>": (16, ChiralType.CHI_TETRAHEDRAL_CW, 0, 0),
+    "<S2>": (16, ChiralType.CHI_TETRAHEDRAL_CW, 1, 0),
+    "<S3>": (16, ChiralType.CHI_UNSPECIFIED, -1, 0),
+    "<S4>": (16, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<S5>": (16, ChiralType.CHI_UNSPECIFIED, 1, 0),
+    "<S6>": (16, ChiralType.CHI_UNSPECIFIED, 1, 1),
+    "<Cl0>": (17, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<Br0>": (35, ChiralType.CHI_UNSPECIFIED, 0, 0),
+    "<I0>": (53, ChiralType.CHI_UNSPECIFIED, 0, 0),
 }
-BOND_TOKENS = {
-    "<:down>": ("AROMATIC", "ENDDOWNRIGHT"),
-    "<:up>": ("AROMATIC", "ENDUPRIGHT"),
-    "<:>": ("AROMATIC", "NONE"),
-    "<=>": ("DOUBLE", "NONE"),
-    "<-down>": ("SINGLE", "ENDDOWNRIGHT"),
-    "<-up>": ("SINGLE", "ENDUPRIGHT"),
-    "<->": ("SINGLE", "NONE"),
-    "<#>": ("TRIPLE", "NONE"),
+ATOMFEAT2TOKEN = {val: key for key, val in TOKEN2ATOMFEAT.items()}
+TOKEN2BONDFEAT = {
+    "<:down>": (BondType.AROMATIC, BondDir.ENDDOWNRIGHT),
+    "<:up>": (BondType.AROMATIC, BondDir.ENDUPRIGHT),
+    "<:>": (BondType.AROMATIC, BondDir.NONE),
+    "<=>": (BondType.DOUBLE, BondDir.NONE),
+    "<-down>": (BondType.SINGLE, BondDir.ENDDOWNRIGHT),
+    "<-up>": (BondType.SINGLE, BondDir.ENDUPRIGHT),
+    "<->": (BondType.SINGLE, BondDir.NONE),
+    "<#>": (BondType.TRIPLE, BondDir.NONE),
 }
-
-CHIRAL_TAG_DICT = {
-    "CHI_UNSPECIFIED": Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
-    "CHI_TETRAHEDRAL_CW": Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
-    "CHI_TETRAHEDRAL_CCW": Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
-    "CHI_OTHER": Chem.rdchem.ChiralType.CHI_OTHER,
-}
-
-BOND_TYPE_DICT = {
-    "SINGLE": Chem.rdchem.BondType.SINGLE,
-    "DOUBLE": Chem.rdchem.BondType.DOUBLE,
-    "TRIPLE": Chem.rdchem.BondType.TRIPLE,
-    "AROMATIC": Chem.rdchem.BondType.AROMATIC,
-}
-
-BOND_DIR_DICT = {
-    "NONE": Chem.rdchem.BondDir.NONE,
-    "ENDUPRIGHT": Chem.rdchem.BondDir.ENDUPRIGHT,
-    "ENDDOWNRIGHT": Chem.rdchem.BondDir.ENDDOWNRIGHT,
-}
-
+BONDFEAT2TOKEN = {val: key for key, val in TOKEN2BONDFEAT.items()}
 def get_atom_token(atom):
     feature = (
-        atom.GetAtomicNum(), 
-        str(atom.IsAromatic()), 
-        str(atom.GetChiralTag()), 
+        atom.GetAtomicNum(),
+        atom.GetChiralTag(), 
         atom.GetFormalCharge(), 
         atom.GetNumExplicitHs()
     )
-    return ATOMFEATURE2TOKEN[feature]
+    return ATOMFEAT2TOKEN[feature]
 
 def get_bond_token(bond):
-    feature = str(bond.GetBondType()), str(bond.GetBondDir())
-    return BONDFEATURE2TOKEN[feature]
+    feature = bond.GetBondType(), bond.GetBondDir()
+    return BONDFEAT2TOKEN[feature]
 
-def smiles_to_nx(smiles):
+def smiles2molgraph(smiles):
     mol = Chem.MolFromSmiles(smiles)
     G = nx.Graph()
     for atom in mol.GetAtoms():
@@ -100,29 +81,29 @@ def smiles_to_nx(smiles):
 
     return G
 
-def nx_to_smiles(G):
+def molgraph2smiles(G):
     node_tokens = nx.get_node_attributes(G, "token")
     edge_tokens = nx.get_edge_attributes(G, "token")
     
     mol = Chem.RWMol()
     node_to_idx = dict()
     for node in G.nodes():
-        atomic_num, chiral_tag, formal_charge, num_explicit_Hs = node_tokens[node]
+        atomic_num, chiral_tag, formal_charge, num_explicit_Hs = TOKEN2ATOMFEAT[node_tokens[node]]
         a=Chem.Atom(atomic_num)
-        a.SetChiralTag(CHIRAL_TAG_DICT[chiral_tag])
+        a.SetChiralTag(chiral_tag)
         a.SetFormalCharge(formal_charge)
         a.SetNumExplicitHs(num_explicit_Hs)
         idx = mol.AddAtom(a)
         node_to_idx[node] = idx
 
     for edge in G.edges():
-        bond_type, bond_dir = edge_tokens[edge]
+        bond_type, bond_dir = TOKEN2BONDFEAT[edge_tokens[edge]]
         first, second = edge
         ifirst = node_to_idx[first]
         isecond = node_to_idx[second]
 
-        mol.AddBond(ifirst, isecond, BOND_TYPE_DICT[bond_type])
-        mol.GetBondBetweenAtoms(ifirst, isecond).SetBondDir(BOND_DIR_DICT[bond_dir])
+        mol.AddBond(ifirst, isecond, bond_type)
+        mol.GetBondBetweenAtoms(ifirst, isecond).SetBondDir(bond_dir)
 
     smiles = Chem.MolToSmiles(mol)
     return smiles
